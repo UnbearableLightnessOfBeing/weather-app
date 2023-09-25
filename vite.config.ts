e0597fe0ea273e9d27aa727af5fea9b25b5e3976
Pavel Golomazdin <golomazdinp@gmail.com>
2023-09-25 18:28:55 +0300

    initial setup

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    server: {
+        port: 3000,
+    },
+
+    plugins: [
+        vue(),
+        Components({
+            dts: true,
+            dirs: ["./src/components"],
+        }),
+        AutoImport({
+            dts: true,
+            imports: "vue",
+        }),
+    ],
+});
